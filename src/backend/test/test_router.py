84ce558d835099b52ad191c5f0e8e024f40496cc
nheeheehee <jamielpln@gmail.com>
2022-10-29 23:32:28 +0800

    add inference and deploy via fastAPI

---
+++ src/backend/test/test_router.py
@@ -0,0 +1,14 @@
+"""This is the test for backend API"""
+
+def test_base(test_app):
+    """Test response for base endpoint"""
+
+    response = test_app.get('/cake_classifier')
+    assert response.status_code == 200
+    assert response.json() == {'Model Name': "resnet 50 + mobilenet"}
+
+def test_env(test_app):
+    '''Test response for env endpoint'''
+    response = test_app.get('/env')
+    assert response.status_code == 200
+    assert response.json() == {'Env': 'testing'}
